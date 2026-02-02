f22f40358645787a584da3419f9a40f294605e6a
Moses Divaker <moses.divaker@gmail.com>
2026-02-01 23:46:36 -0800

    chore: Release Clawgate v0.1.1

--- src/index.test.ts
+++ src/index.test.ts
@@ -30,7 +30,7 @@ describe('Clawgate MVP', () => {
 
     expect(res.status).toBe(200)
     expect(data.ok).toBe(true)
-    expect(data.version).toBe('0.1.0')
+    expect(data.version).toBe('0.1.1')
   })
 
   test('POST /v1/exec with allowed command succeeds', async () => {
